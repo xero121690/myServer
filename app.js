a894856ec52fe4a4e022b82bd63037fe981d11b4
Misael Vargas <31321816+xero121690@users.noreply.github.com>
2023-09-12 21:38:32 -0700

    reverted everything back

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
  
-
-const port = 80;
+const hostname = '127.0.0.1';
+const port = 3000;
  
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
  
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
