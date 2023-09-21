b802b3716f7fb0aa9533e95c513ba8e88c67288b
Misael Vargas <31321816+xero121690@users.noreply.github.com>
2023-09-21 07:12:08 -0700

    added sql; post & get

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express');
+import express from 'express'
+import mysql2 from 'mysql2';
 const app = express();
 const port = 3000;
-var cors = require('cors');
+import cors from 'cors'
 
 app.use(cors());
+
+const db = mysql2.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"mysql123",
+    database:"timetracker",
+    //node mysql module casts  all mysql date/datetime data types into JS date objects
+    //hence dateStrings: true to prevent it from using datetime format on return
+    dateStrings: true 
+})
 
 let interval; // Declare the interval variable
 let seconds = 0; // Initialize seconds counter
@@ -28,21 +39,45 @@ app.get('/start', (req, res) => {
 });
 
 // Route to stop the continuous function and return the seconds elapsed
-app.get('/stop', (req, res) => {
+/*******************************************************************************************************************push_time************************************************************************************************************************************************** */
+app.post('/stop', (req, res) => {
+    
     if (interval) {
         clearInterval(interval); // Stop the interval if it's running
         interval = null; // Reset the interval variable
         const elapsedSeconds = seconds; // Store the elapsed seconds
         
         seconds = 0; // Reset the seconds counter
-        res.send(`Continuous function stopped. Seconds elapsed: ${elapsedSeconds}`);
+
+
+        //MYSQL Query
+         //DATETIME values in 'YYYY-MM-DD hh:mm:ss'
+        let datetime = new Date();
+
+
+        const   q = "INSERT INTO userdata (`seconds`, `UserID`, `date`) VALUES (?)"
+
+        const values = [elapsedSeconds, 1, datetime.toISOString().slice(0, 10)]
+
+        db.query(q, [values], (err, data)=> {
+            if (err) return res.json(err)
+            return res.send(`Continuous function stopped. Seconds elapsed: ${elapsedSeconds}`)
+        })
+
     } else {
         res.send('Continuous function is not running.');
     }
 });
 
 app.get('/retrieve', (req, res) => {
-        res.send(`${totalTime}`);
+        // UserID retrieve search
+        const q = "SELECT * FROM userdata WHERE UserID=1"
+
+        db.query(q, (err, data)=> {
+            if (err) return res.json(err)
+            return res.send(data)
+        })
+    
 });
 
 
